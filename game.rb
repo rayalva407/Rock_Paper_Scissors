98b5bc9aacd152e1cc281cc63a72c34b7b9cab21
rayalva407 <rayalva407@gmail.com>
2021-02-07 19:43:40 -0700

    adds user input and computer play

--- game.rb
+++ game.rb
@@ -3,4 +3,8 @@ class Game
   puts "Welcome! Let's play Rock, Paper, Scissors."
   puts "--------------------------------------------"
   puts "Type Rock, Paper, or Scissors to make your selection"
+  
+  human_play = gets.chomp.downcase
+  computer_play = computer_choices.sample.downcase
+  puts "----------------------------------------------"
 end
