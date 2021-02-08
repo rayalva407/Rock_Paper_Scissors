387e1b6d472f56112f164a52eb641868020fc515
rayalva407 <rayalva407@gmail.com>
2021-02-07 19:53:08 -0700

    adds conditions for game outcome

--- game.rb
+++ game.rb
@@ -3,8 +3,19 @@ class Game
   puts "Welcome! Let's play Rock, Paper, Scissors."
   puts "--------------------------------------------"
   puts "Type Rock, Paper, or Scissors to make your selection"
-  
+
   human_play = gets.chomp.downcase
   computer_play = computer_choices.sample.downcase
   puts "----------------------------------------------"
+
+  if human_play == computer_play
+    puts "Computer chose #{computer_play.capitalize}"
+    puts "You Tied! Try Again"
+  elsif human_play == "rock" && computer_play == "scissors" || human_play == "paper" && computer_play == "r" || human_play == "s" && computer_play == "p"
+    puts "Computer chose #{computer_play.capitalize}"
+    puts "You win!"
+  else
+    puts "Computer chose #{computer_play.capitalize}"
+    puts "You lose :( Try Again"
+  end
 end
